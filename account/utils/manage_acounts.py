b2a5251432e8ae2ac7e459bf4ddeea2f48ae48e1
Víctor Hugo Herver Segura <vicherver@gmail.com>
2023-10-29 20:38:12 -0600

    Linter and documentation

--- account/utils/manage_acounts.py
+++ account/utils/manage_acounts.py
@@ -1,6 +1,30 @@
 from account.models import Account
 
 
-def get_account_id(email):
+def get_account(email: str) -> Account:
+    """
+    Retrieve or create an Account object based on the provided email.
+
+    Parameters:
+    email (str): The email address associated with the Account.
+
+    Description:
+    This function attempts to retrieve an existing Account object
+    from the database based on the provided email. If the Account
+    does not exist, it creates a new Account object and returns it.
+
+    Args:
+    - email (str):
+     A string representing the email address associated with the Account.
+
+    Returns:
+    Account: The Account object associated with the provided email.
+    If it doesn't exist, a new Account object is created and returned.
+
+    Type Annotations:
+    - email (str): The function expects a string representing an email address.
+    - Returns (Account): The function returns an Account object.
+    """
+
     object, _ = Account.objects.get_or_create(email=email)
-    return object.id
+    return object
